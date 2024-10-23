6e23fd4e417f3a218909d175082f9967e8b8355a
darkstussy <chornij.stas@gmail.com>
2024-10-23 11:00:27 +0300

    bot started

--- crypto_checker/utils/notify.py
+++ crypto_checker/utils/notify.py
@@ -3,4 +3,4 @@ from aiogram import Bot
 
 async def notify_admins(bot: Bot, admins: list[int]):
     for admin in admins:
-        await bot.send_message(admin, "Бот запущен")
+        await bot.send_message(admin, "Bot started")
